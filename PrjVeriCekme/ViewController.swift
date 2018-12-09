24f2397019ca0ba776a5b699408e0968993f35b1
Demet KIZILBUĞA <demetkizilbuga@gmail.com>
2018-12-09 16:14:55 +0300

    Initial Commit

---
+++ PrjVeriCekme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrjVeriCekme
+//
+//  Created by Demet KIZILBUĞA on 9.12.2018.
+//  Copyright © 2018 Demet KIZILBUĞA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
